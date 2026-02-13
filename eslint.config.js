c5b795c745aeb70c379f1310438058c8ab4455f5
nils-keller-dev <keller.nils99@gmail.com>
2026-02-13 23:45:14 +0100

    🔧 fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,16 +4,18 @@ import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import globals from 'globals';
 
 export default defineConfig([
+	js.configs.recommended,
 	{
 		files: ['**/*.js'],
-		plugins: { js },
-		extends: ['js/recommended'],
 		languageOptions: {
 			ecmaVersion: 'latest',
 			sourceType: 'module',
 			globals: {
 				...globals.browser,
 			},
+		},
+		rules: {
+			'prefer-const': 'error',
 		},
 	},
 	eslintConfigPrettier,
